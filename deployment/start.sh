79df2a9831d04684fef68a6330bea8ea123e775f
geri <zeqirigeri@gmail.com>
2019-04-20 14:47:12 +0200

    -start up command for docker container added command to call corebos updater -travis config changes

--- deployment/start.sh
+++ deployment/start.sh
@@ -4,4 +4,7 @@ build/HelperScripts/createuserfiles
 
 chown -R www:www /www/user_privileges
 
+php modules/cbupdater/loadapplychanges.php 
+php modules/cbupdater/loadapplychanges.php 
+
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
