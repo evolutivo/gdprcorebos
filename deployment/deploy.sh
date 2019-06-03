5373196fb2eb6f72b27db5e5b18d9e55512f9a02
geri <zeqirigeri@gmail.com>
2019-06-03 23:51:59 +0200

    - fix max client request size - apply new tag to kubernetes deployment for corebos admin

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,3 +6,7 @@ docker push spikelabs/corebos-gdpr:latest
 
 docker push spikelabs/corebos-gdpr:$SHA
 
+curl --header "Content-Type: application/json" \
+ --request POST \
+ --data '{"token": "'"$COREBOS_ADMIN_TOKEN"'","image_tag":"spikelabs/corebos-gdpr:'"$SHA"'"}' \
+ https://k8admin.evolutivo.it/api/update_client_image
