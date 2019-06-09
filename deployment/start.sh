8a811d5c55844cd32ee15b43a69665982a2092b0
geri <zeqirigeri@gmail.com>
2019-06-09 22:38:27 +0200

    preserve initial images from storage folder

--- deployment/start.sh
+++ deployment/start.sh
@@ -5,6 +5,9 @@ build/HelperScripts/createuserfiles
 chown -R www:www /www/user_privileges
 
 php modules/cbupdater/loadapplychanges.php 
-php modules/cbupdater/loadapplychanges.php 
+php modules/cbupdater/loadapplychanges.php
+
+cp -r /tmp_storage/. /www/storage
+chown -R www:www /www/storage
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
