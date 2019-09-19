a8a0c9c3f000047669c0c2b67f3733ca83fe561a
Ernest Vila <vila.ernest@gmail.com>
2019-09-19 16:23:19 +0200

    feat(project) testing size of gatt chart

--- modules/Project/BURAK_Gantt.class.php
+++ modules/Project/BURAK_Gantt.class.php
@@ -403,7 +403,7 @@ class BURAK_Gantt {
 		$this->gantt_width = ((($this->gantt_end+1 - $this->gantt_start) / 86400) * $this->inc_x) + $this->inc_x +1;
 
 		// create image
-		$this->im = imagecreatetruecolor(863, 480);
+		$this->im = imagecreatetruecolor(480, 480);
 		
 		//$this->im = imagecreatetruecolor($this->gantt_width,$this->gantt_height);
 
