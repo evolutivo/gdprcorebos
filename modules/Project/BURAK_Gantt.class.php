12e767d02dbce877a5bf0df6b2aa07fbf38ed42c
Ernest Vila <vila.ernest@gmail.com>
2019-09-25 12:24:14 +0200

    feat(project) Testing font task

--- modules/Project/BURAK_Gantt.class.php
+++ modules/Project/BURAK_Gantt.class.php
@@ -121,7 +121,7 @@ class BURAK_Gantt {
 		$this->heights["month"] = 20;
 		$this->heights["day"] = 10;
 		$this->heights["group"] = 3;
-		$this->heights["task"] = 12;
+		$this->heights["task"] = 20;
 		$this->heights["progress"] = 4;
 		$this->inc_y = $this->heights["task"] + 18;
 		$this->font = 1;
