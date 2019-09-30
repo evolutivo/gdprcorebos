02e5fe09d75a0304295b852607a813a77cf1d95e
Ernest Vila <vila.ernest@gmail.com>
2019-09-30 12:36:29 +0200

    feat(project) Testing getWeekendDates

--- include/js/general.js
+++ include/js/general.js
@@ -916,7 +916,7 @@ function getWeekendsFromYear(year) {
     method: 'POST',
     url: 'index.php?'+url
   }).done(function (response) {
-    console.log(response.text);    
+    console.log(response);    
   });
 }
 
