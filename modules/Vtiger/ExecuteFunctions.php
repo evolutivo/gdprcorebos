61f11bf74e2e6e3949a19ab3279f2d3086ca0102
Ernest Vila <vila.ernest@gmail.com>
2019-09-30 16:38:32 +0200

    feat(project) Testing getWeekendDates

--- modules/Vtiger/ExecuteFunctions.php
+++ modules/Vtiger/ExecuteFunctions.php
@@ -130,7 +130,8 @@ switch ($functiontocall) {
 		$startDate = vtlib_purify($_REQUEST['startFrom']);
 		$endDate = vtlib_purify($_REQUEST['endFrom']);
 		$format = 'Y-m-d';
-		$results =  DateTimeField::getWeekendDates($startDate, $endDate, $format);		
+		$results =  DateTimeField::getWeekendDates($startDate, $endDate, $format);
+		echo "";
 		break;
 	case 'ValidationLoad':
 		$valmod = vtlib_purify($_REQUEST['valmodule']);
