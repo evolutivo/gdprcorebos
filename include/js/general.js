2f581d048c35e32fc9f472aad04846b1fed1dc53
Ernest Vila <vila.ernest@gmail.com>
2019-09-30 18:40:11 +0200

    feat(Project) Testing....

--- include/js/general.js
+++ include/js/general.js
@@ -916,7 +916,7 @@ function getWeekendsFromYear(year) {
     method: 'POST',
     url: 'index.php?'+url
   }).done(function (response) {
-    var jArray = <?php echo json_encode($phpArray ); ?>;
+    var jArray = json_encode(response);
     for(var i=0;i<12;i++){
        weekdays.push(jArray[i]);
    }
