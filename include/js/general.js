22ce094c11a68611667ac0aa3ead90087bc796d6
Ernest Vila <vila.ernest@gmail.com>
2019-10-18 14:57:52 +0200

    feat(Project) Testing TimeControl Without Festivity

--- include/js/general.js
+++ include/js/general.js
@@ -497,7 +497,7 @@ function loadEMPXWPData(emp_x_wp_worksheet, wpXMonth, festivity, weekEndsForYear
                 3- Remove day from calendar
             ***************************************/
             console.log("First Time: ", day);
-            let anotherDay = randomDay[Math.floor(Math.random() * randomDay.length)];
+            var anotherDay = randomDay[Math.floor(Math.random() * randomDay.length)];
             console.log("Second Time: ", anotherDay);
 
 		}
