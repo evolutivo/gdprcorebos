10a98909a1707ad35ea4f9340434df92a6087401
Ernest Vila <vila.ernest@gmail.com>
2019-10-18 14:54:57 +0200

    feat(Project) Testing TimeControl Without Festivity

--- include/js/general.js
+++ include/js/general.js
@@ -477,92 +477,60 @@ function loadEMPXWPData(emp_x_wp_worksheet, wpXMonth, festivity, weekEndsForYear
 
 		randomDay = getRandomDayPerMonthPerWPPerEMP(finalReport[r]["Employee"], month, employeeCalendars);
 		var day = randomDay[Math.floor(Math.random() * randomDay.length)];
+        console.log("First Time: ", day);
 
-
-		if (day == null || day.length == 0) {
-		    console.log(day + "-" + month);
-
-		   /* for (let i = 0; i < mesiLavorativi.length; i++) {
+		if (day == null || day.length === 0) {
+		    /*for (var i = 0; i < mesiLavorativi.length; i++) {
 		        if (mesiLavorativi[i].Mese == month) {
 		            mesiLavorativi.splice(i,1);
 		        }
-		    }*/
+		    }
+
+            otherRandomMonth = getMeseRandomPerWorkPackPerEMP(finalReport[r]["Employee"], finalReport[r]["WP Code"], mesiLavorativi);
+			var otherMonth = otherRandomMonth[Math.floor(Math.random() * otherRandomMonth.length)];
+			copyRandomDay = getRandomDayPerMonthPerWPPerEMP(finalReport[r]["Employee"], otherMonth, employeeCalendars);
+			var otherDay = copyRandomDay[Math.floor(Math.random() * copyRandomDay.length)];*/
+
 		    /*************************************
 		        1- Generate anotherMonth
                 2- Generate anotherDay
                 3- Remove day from calendar
             ***************************************/
+            var anotherDay = randomDay[Math.floor(Math.random() * randomDay.length)];
+            console.log("First Time: ", anotherDay);
 
-            otherRandomMonth = getMeseRandomPerWorkPackPerEMP(finalReport[r]["Employee"], finalReport[r]["WP Code"], mesiLavorativi);
-            let otherMonth = otherRandomMonth[Math.floor(Math.random() * otherRandomMonth.length)];
-            copyRandomDay = getRandomDayPerMonthPerWPPerEMP(finalReport[r]["Employee"], otherMonth, employeeCalendars);
-            let otherDay = copyRandomDay[Math.floor(Math.random() * copyRandomDay.length)];
+		}
 
-            let newObj =
-                {
-                    ProjectID: finalReport[r].ProjectID,
-                    ProjectTaskID: finalReport[r].ProjectTaskID,
-                    Employee: finalReport[r].Employee,
-                    WPCode: finalReport[r]["WP Code"],
-                    WorkingHour: finalReport[r]["WorkingHour"],
-                    RealWorkingHour: 0,
-                    TotalHoursWorked: finalReport[r]["Total Hours Worked"],
-                    DateStart: finalReport[r]["Start Time"],
-                    DateEnd: finalReport[r]["End Time"],
-                    Percentage: finalReport[r].Percentuale,
-                    PercentageWP: finalReport[r].PercentualeTotaleWP + '%',
-                    TotalHoursWP: finalReport[r].TotHoursXWP,
-                    TotaleWorkDaysWP: finalReport[r].TotWDxWP,
-                    //Day: (day == null || day.length === 0) ? otherDay : day,
-                    Day: otherDay,
-                    Month: month,
-                    Year: yearOfReference
-                }
+		var obj =
+		{
+			ProjectID: finalReport[r].ProjectID,
+    	    ProjectTaskID: finalReport[r].ProjectTaskID,
+			Employee: finalReport[r].Employee,
+			WPCode: finalReport[r]["WP Code"],
+			WorkingHour: finalReport[r]["WorkingHour"],
+			RealWorkingHour: 0,
+			TotalHoursWorked: finalReport[r]["Total Hours Worked"],
+			DateStart: finalReport[r]["Start Time"],
+			DateEnd: finalReport[r]["End Time"],
+			Percentage: finalReport[r].Percentuale,
+			PercentageWP: finalReport[r].PercentualeTotaleWP + '%',
+			TotalHoursWP: finalReport[r].TotHoursXWP,
+			TotaleWorkDaysWP: finalReport[r].TotWDxWP,
+			Day: (day == null || day.length === 0) ? anotherDay : day,
+            // Day: (day == null || day.length === 0) ? '' : day,
+			Month: month,
+			Year: yearOfReference
+		}
 
-            output.push(newObj);
+		output.push(obj);
 
-            //Remove Record from EmployeeCalendar
-            // removeDayOfMonthFromEmployeeCalendar(finalReport[r].Employee, month, day, employeeCalendars);
-            for (var v = 0; v < employeeCalendars.length; v++) {
-                if (employeeCalendars[v].EMP == finalReport[r].Employee && employeeCalendars[v].Mese == otherMonth && employeeCalendars[v].Giorno == otherDay) {
-                    employeeCalendars.splice(v, 1);
-                }
-            }
-
-		}else{
-            var obj =
-                {
-                    ProjectID: finalReport[r].ProjectID,
-                    ProjectTaskID: finalReport[r].ProjectTaskID,
-                    Employee: finalReport[r].Employee,
-                    WPCode: finalReport[r]["WP Code"],
-                    WorkingHour: finalReport[r]["WorkingHour"],
-                    RealWorkingHour: 0,
-                    TotalHoursWorked: finalReport[r]["Total Hours Worked"],
-                    DateStart: finalReport[r]["Start Time"],
-                    DateEnd: finalReport[r]["End Time"],
-                    Percentage: finalReport[r].Percentuale,
-                    PercentageWP: finalReport[r].PercentualeTotaleWP + '%',
-                    TotalHoursWP: finalReport[r].TotHoursXWP,
-                    TotaleWorkDaysWP: finalReport[r].TotWDxWP,
-                    //Day: (day == null || day.length === 0) ? otherDay : day,
-                    Day: day,
-                    Month: month,
-                    Year: yearOfReference
-                }
-
-            output.push(obj);
-
-            //Remove Record from EmployeeCalendar
-            // removeDayOfMonthFromEmployeeCalendar(finalReport[r].Employee, month, day, employeeCalendars);
-            for (var v = 0; v < employeeCalendars.length; v++) {
-                if (employeeCalendars[v].EMP == finalReport[r].Employee && employeeCalendars[v].Mese == month && employeeCalendars[v].Giorno == day) {
-                    employeeCalendars.splice(v, 1);
-                }
-            }
-        }
-
-
+		//Remove Record from EmployeeCalendar
+		// removeDayOfMonthFromEmployeeCalendar(finalReport[r].Employee, month, day, employeeCalendars);
+		for (var v = 0; v < employeeCalendars.length; v++) {
+			if (employeeCalendars[v].EMP == finalReport[r].Employee && employeeCalendars[v].Mese == month && employeeCalendars[v].Giorno == day) {
+				employeeCalendars.splice(v, 1);
+			}
+		}
 	}
 
     console.log("Employee Calendar After Random Generation: ", employeeCalendars);
