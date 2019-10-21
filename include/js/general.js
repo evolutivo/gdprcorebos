7bd70f0bd0b286056ef6857a452ddf95116727d1
Ernest Vila <vila.ernest@gmail.com>
2019-10-21 17:10:43 +0200

    Testing exclude festivity from timecontrol

--- include/js/general.js
+++ include/js/general.js
@@ -878,7 +878,7 @@ function dateRange(startDate, endDate, wp_code, emp) {
 */
 function formatDate (input) {
     var datePart = input.match(/\d+/g),
-    year = datePart[0].substring(2), // get only two digits
+    year = datePart[0].substring(4), // get only two digits
     month = datePart[1], day = datePart[2];
     return day+'-'+month+'-'+year;
 }
